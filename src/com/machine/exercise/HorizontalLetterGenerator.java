ff70dfb84d22b3719d5a8f428a430ca29edd262c
Paolo Severino <pseverino@mssngsolutions.ph>
2018-10-10 14:58:35 +0800

    print horizontally

--- src/com/machine/exercise/HorizontalLetterGenerator.java
+++ src/com/machine/exercise/HorizontalLetterGenerator.java
@@ -2,29 +2,32 @@ package com.machine.exercise;
 
 public class HorizontalLetterGenerator {
 
-    static int gridSize = 5;
+    static int gridSize = 9;
     static String[] letters = {"X", "y", "z"};
-    static char[][] layout = new char[gridSize][gridSize * letters.length];
+    static int yGridTotal = gridSize * letters.length;
+    static char[][] layout = new char[gridSize][yGridTotal];
+
 
     public static void main(String[] args) throws Exception {
+
         LetterFactory factory = new LetterFactory(gridSize);
 
+        int count = 0;
         for (String s: letters) {
             char[][] c = factory.getLetter(s).buildLetter();
-            printHorizontally(c);
-        }
-    }
-
-    // TODO
-    static void printHorizontally(char[][] letter) {
-
-        for (int i = 0; i < layout.length; i++) {
-            for (int j = 0; j < letter[i].length; j++) {
-                layout[i][j] = '*';
-            }
-
+            printHorizontally(c, count);
+            count += yGridTotal/letters.length;
         }
 
         Letter.printVertically(layout);
+    }
+
+    static void printHorizontally(char[][] letter, int columnStart) {
+
+        for (int i = 0; i < layout.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                layout[i][j+columnStart] = letter[i][j];
+            }
+        }
     }
 }
